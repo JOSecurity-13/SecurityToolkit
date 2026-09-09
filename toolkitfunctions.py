f15fd7efdd99e91b5af1573552e801d3df6dca14
Jackson O'Daniel <jackson@jo-security.com>
2026-09-08 20:23:20 -0700

    Update to fix error in last commit.

--- toolkitfunctions.py
+++ toolkitfunctions.py
@@ -54,7 +54,6 @@ def specialCheck(password: str) -> str:
 
 
 # def breachCheck(password: str) -> str:
-
 def passStrAnalyzer():
 
     print("\n---------Password Analyzer---------\n")
@@ -78,28 +77,5 @@ def passStrAnalyzer():
     elif strengthSelection == "x":
         print("\n Exiting Password Analyzer \n")
 
-
-
-
-
-
-# password strength analyzer
-def lengthCheck(password: str) -> str:
-    if len(password) < 8:
-        print(bcolors.WARNING + "Your password is too short" + bcolors.ENDC)
-    elif 14 <= len(password) <= 64:
-        print(bcolors.OKCYAN + "Your password meets NIST guidelines for length!" + bcolors.ENDC)
-    elif len(password) > 64:
-        print(bcolors.WARNING + "Your password is too long" + bcolors.ENDC)
-
-
-    print("\n---------Password Analyzer---------\n")
-    print("l - length")
-    print("e - entropy/complexity")
-
-    strengthSelection = input("based on the above options what would you like to analyze your password for? ")
-    if strengthSelection == "l":
-        lengthCheck(userPassword)
-    elif strengthSelection == "e":
 
 
